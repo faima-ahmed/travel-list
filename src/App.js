310b35479334fd8a23434730c32ba511c5e7cbce
faima-ahmed <faimaahmeddiya@gmail.com>
2024-06-02 11:25:27 +0600

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Logo from "./Logo";
+import Form from "./Form";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -44,52 +45,6 @@ function handleClearList(){
       />
       <Stats items={items}/>
     </div>
-  );
-}
-
-function Form({onAddItems}) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
- 
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = {
-      description,
-      quantity,
-      packed: false,
-      id: Date.now(),
-    };
-    console.log(newItem);
-
-    onAddItems(newItem);
-
-    setDescription("");
-    setQuantity(1);
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-
-      <button>Add</button>
-    </form>
   );
 }
 
