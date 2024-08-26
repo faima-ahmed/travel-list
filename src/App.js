f00980551bc3904749efc0e194445631dbc7b74e
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-26 08:03:30 +0600

    Commit 1

--- src/App.js
+++ src/App.js
@@ -13,11 +13,16 @@ export default function App() {
   function handleAddItem(item){
     setItems ((items)=>[...items, item]);
 }
+ 
+function handleDeleteItem(id){
+  setItems ((items)=> items.filter((item) => item.id !== id))
+}
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItem}/>
-      <PackingList items={items}/>
+      <PackingList items={items} onDeleteItem={handleDeleteItem}/>
       <Stats />
     </div>
   );
@@ -73,25 +78,25 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingList({items}) {
+function PackingList({items, onDeleteItem}) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={()=> onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
